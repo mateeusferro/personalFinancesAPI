0939978964528b30d0971e936101aefb51d39419
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-10 22:56:45 -0300

    Adding a new file to the blacklist, I am using a HashSet to store the tokens. I use it to verify whether a token is valid or not. However, I am aware that this approach is not recommended for production

---
+++ src/main/java/com/mateeusferro/backend/utils/Blacklist.java
@@ -0,0 +1,20 @@
+package com.mateeusferro.backend.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class Blacklist {
+
+    private final Set<String> blacklist = new HashSet<>();
+
+    public void addToBlacklist(String token) {
+        blacklist.add(token);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return blacklist.contains(token);
+    }
+}
