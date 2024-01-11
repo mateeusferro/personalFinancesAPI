a8aa1b2a166e935b3bb034695e31100829dd1c7e
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-10 22:53:31 -0300

    New custom exception to verify if  jwt token is null

---
+++ src/main/java/com/mateeusferro/backend/exceptions/TokenNullException.java
@@ -0,0 +1,7 @@
+package com.mateeusferro.backend.exceptions;
+
+public class TokenNullException extends NullPointerException{
+    public TokenNullException(String message) {
+        super(message);
+    }
+}
