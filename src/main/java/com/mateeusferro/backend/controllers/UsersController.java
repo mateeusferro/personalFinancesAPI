478fdceac423c99c096852d4a0257902f148aa61
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-15 22:31:22 -0300

    New functions to update users

--- src/main/java/com/mateeusferro/backend/controllers/UsersController.java
+++ src/main/java/com/mateeusferro/backend/controllers/UsersController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -22,8 +19,15 @@ public class UsersController {
 
     @PostMapping("/create")
     public ResponseEntity createUser(@RequestBody @Valid UsersDTO usersDTO){
-        usersService.create(usersDTO);
+        usersService.createUser(usersDTO);
         ResponseDTO responseDTO = new ResponseDTO("User created", HttpStatus.CREATED);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateUser(@PathVariable long id, @RequestBody @Valid UsersDTO usersDTO) {
+        usersService.updateUser(id, usersDTO);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseDTO("User updated",
+                HttpStatus.NO_CONTENT));
     }
 }
--- src/main/java/com/mateeusferro/backend/services/UsersService.java
+++ src/main/java/com/mateeusferro/backend/services/UsersService.java
@@ -1,8 +1,11 @@
 package com.mateeusferro.backend.services;
 
+import com.mateeusferro.backend.dtos.SalaryDTO;
 import com.mateeusferro.backend.dtos.UsersDTO;
 import com.mateeusferro.backend.enums.UserRole;
+import com.mateeusferro.backend.exceptions.ResourceNotFoundException;
 import com.mateeusferro.backend.exceptions.UserAlreadyExistsException;
+import com.mateeusferro.backend.models.Salary;
 import com.mateeusferro.backend.models.Users;
 import com.mateeusferro.backend.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,7 @@ public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
 
-    public void create(UsersDTO usersDTO){
+    public void createUser(UsersDTO usersDTO){
         if(this.usersRepository.findByEmail(usersDTO.email()) != null)
             throw new UserAlreadyExistsException("This email already exists");
 
@@ -24,5 +27,17 @@ public class UsersService {
         Users newUser = new Users(usersDTO.name(), usersDTO.email(), encryptedPassword, "user");
 
         this.usersRepository.save(newUser);
+    }
+
+    public void updateUser(long id, UsersDTO usersDTO){
+        Users updateUsers = usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id: " + id));
+        String encryptedPassword = new BCryptPasswordEncoder().encode(usersDTO.password());
+
+        updateUsers.setName(usersDTO.name());
+        updateUsers.setEmail(usersDTO.email());
+        updateUsers.setPassword(encryptedPassword);
+
+        usersRepository.save(updateUsers);
     }
 }
