a69b77035d3381ff71165cc888b6edf22500f4f7
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-15 22:31:13 -0300

    New functions to update salary

--- src/main/java/com/mateeusferro/backend/controllers/SalaryController.java
+++ src/main/java/com/mateeusferro/backend/controllers/SalaryController.java
@@ -1,5 +1,6 @@
 package com.mateeusferro.backend.controllers;
 
+import com.mateeusferro.backend.dtos.InvestmentsDTO;
 import com.mateeusferro.backend.dtos.ResponseDTO;
 import com.mateeusferro.backend.dtos.SalaryDTO;
 import com.mateeusferro.backend.services.SalaryService;
@@ -8,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/salary")
@@ -20,9 +19,16 @@ public class SalaryController {
     private SalaryService salaryService;
 
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody @Valid SalaryDTO salaryDTO){
+    public ResponseEntity createSalary(@RequestBody @Valid SalaryDTO salaryDTO){
         salaryService.createSalary(salaryDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO("Salary created",
                 HttpStatus.CREATED));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateSalary(@PathVariable long id, @RequestBody @Valid SalaryDTO salaryDTO) {
+        salaryService.updateSalary(id, salaryDTO);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseDTO("Salary updated",
+                HttpStatus.NO_CONTENT));
     }
 }
--- src/main/java/com/mateeusferro/backend/services/SalaryService.java
+++ src/main/java/com/mateeusferro/backend/services/SalaryService.java
@@ -1,6 +1,9 @@
 package com.mateeusferro.backend.services;
 
+import com.mateeusferro.backend.dtos.InvestmentsDTO;
 import com.mateeusferro.backend.dtos.SalaryDTO;
+import com.mateeusferro.backend.exceptions.ResourceNotFoundException;
+import com.mateeusferro.backend.models.Investments;
 import com.mateeusferro.backend.models.Salary;
 import com.mateeusferro.backend.repositories.SalaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +19,19 @@ public class SalaryService {
         Salary salary = new Salary(salaryDTO.type(), salaryDTO.value(), salaryDTO.date(),
                                     salaryDTO.description(), salaryDTO.usersId(), salaryDTO.currencyId());
         salaryRepository.save(salary);
+    }
+
+    public void updateSalary(long id, SalaryDTO salaryDTO){
+        Salary updateSalary = salaryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Salary not exist with id: " + id));
+
+        updateSalary.setType(salaryDTO.type());
+        updateSalary.setValue(salaryDTO.value());
+        updateSalary.setDate(salaryDTO.date());
+        updateSalary.setDescription(salaryDTO.description());
+        updateSalary.setUsersId(salaryDTO.usersId());
+        updateSalary.setCurrencyId(salaryDTO.currencyId());
+
+        salaryRepository.save(updateSalary);
     }
 }
