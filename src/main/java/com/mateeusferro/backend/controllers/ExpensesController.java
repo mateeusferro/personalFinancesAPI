d551ab71d1ccbf93e4a6dfb30521c12ce0cce4fd
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-15 22:30:19 -0300

    New functions to update expenses

--- src/main/java/com/mateeusferro/backend/controllers/ExpensesController.java
+++ src/main/java/com/mateeusferro/backend/controllers/ExpensesController.java
@@ -1,5 +1,6 @@
 package com.mateeusferro.backend.controllers;
 
+import com.mateeusferro.backend.dtos.BankAccountDTO;
 import com.mateeusferro.backend.dtos.ExpensesDTO;
 import com.mateeusferro.backend.dtos.ResponseDTO;
 import com.mateeusferro.backend.services.ExpensesService;
@@ -8,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/expenses")
@@ -22,7 +21,14 @@ public class ExpensesController {
     @PostMapping("/create")
     public ResponseEntity createExpense(@RequestBody @Valid ExpensesDTO expensesDTO){
         expensesService.createExpense(expensesDTO);
-        return  ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO("Expense create",
+        return  ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO("Expense created",
                 HttpStatus.CREATED));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateExpense(@PathVariable long id, @RequestBody @Valid ExpensesDTO expensesDTO) {
+        expensesService.updateExpense(id, expensesDTO);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseDTO("Expense updated",
+                HttpStatus.NO_CONTENT));
     }
 }
--- src/main/java/com/mateeusferro/backend/services/ExpensesService.java
+++ src/main/java/com/mateeusferro/backend/services/ExpensesService.java
@@ -1,6 +1,8 @@
 package com.mateeusferro.backend.services;
 
 import com.mateeusferro.backend.dtos.ExpensesDTO;
+import com.mateeusferro.backend.exceptions.ResourceNotFoundException;
+import com.mateeusferro.backend.models.BankAccount;
 import com.mateeusferro.backend.models.Expenses;
 import com.mateeusferro.backend.repositories.ExpensesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +18,21 @@ public class ExpensesService {
                                         expensesDTO.paid(), expensesDTO.paidDate(), expensesDTO.paymentType(),
                                         expensesDTO.usersId(), expensesDTO.currencyId());
         expensesRepository.save((expenses));
+    }
+
+    public void updateExpense(long id, ExpensesDTO expensesDTO){
+        Expenses updateExpenses = expensesRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Expense not exist with id: " + id));
+
+        updateExpenses.setType(expensesDTO.type());
+        updateExpenses.setDate(expensesDTO.date());
+        updateExpenses.setValue(expensesDTO.value());
+        updateExpenses.setPaid(expensesDTO.paid());
+        updateExpenses.setPaidDate(expensesDTO.paidDate());
+        updateExpenses.setPaymentType(expensesDTO.paymentType());
+        updateExpenses.setUsersId(expensesDTO.usersId());
+        updateExpenses.setCurrencyId(expensesDTO.currencyId());
+
+        expensesRepository.save(updateExpenses);
     }
 }
